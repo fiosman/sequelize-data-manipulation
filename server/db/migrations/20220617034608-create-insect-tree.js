d671196256d297f7dd4bbd8c76e05a7b078caec3
fiosman <fares.osman@outlook.com>
2022-06-17 23:27:13 -0400

    update join table foreign keys referencing

--- server/db/migrations/20220617034608-create-insect-tree.js
+++ server/db/migrations/20220617034608-create-insect-tree.js
@@ -1,30 +1,44 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('InsectTrees', {
+    await queryInterface.createTable("InsectTrees", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       insectId: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Insects",
+          key: "id",
+        },
+        onDelete: "cascade",
       },
       treeId: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Trees",
+          key: "id",
+        },
+        onDelete: "cascade",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('InsectTrees');
-  }
+    await queryInterface.dropTable("InsectTrees");
+  },
 };
